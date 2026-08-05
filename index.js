150c8af8e32a10a860c6d933080be7b2aa6df34b
Gustavons16 <gustavo123trava@gmail.com>
2026-08-05 00:09:34 +0000

    correção da api

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ import storeRoutes from './src/routes/storeRoutes.js';
 import categoryRoutes from './src/routes/categoryRoutes.js';
 import productRoutes from './src/routes/productRoutes.js';
 import dayofjobRoutes from './src/routes/dayofjobRoutes.js';
-import productRoutes from './routes/productRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
